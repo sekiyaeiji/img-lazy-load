0abc06450d763aab34b37d777b93f551c767d3fe
sekiya <sekiya@gnavi.co.jp>
2019-01-10 16:44:39 +0900

    fixed

--- src/js/lazyloadimg.js
+++ src/js/lazyloadimg.js
@@ -5,7 +5,10 @@ module.exports = function() {
   // lazy load img
   const lazyLoadImg = {
     config: {
-      target_class_name: 'js-lazyLoadImg',
+      class_target: 'js-lazyLoadImg',
+      class_loaded: 'js-loaded',
+      offset_y_loaded: 500,
+      image_default: 'img/default.png',
     },
     // 初期処理
     init: function() {
@@ -15,45 +18,55 @@ module.exports = function() {
       // event bind
       that.event()
     },
-    // イベントbind
+    // イベント bind
     event: function() {
       // window scroll
       window.addEventListener('scroll', lazyLoadImg.main)
     },
+    // イベント unbind
+    eventUnBind: function() {
+      // window scroll
+      window.removeEventListener('scroll', lazyLoadImg.main)
+    },
     // 画像パスをdummy画像パスに差し替え
     ready: function() {
-      const that = this
-      console.log(that.config.target_class_name)
-      const target = document.getElementsByClassName(lazyLoadImg.config.target_class_name)
-      const imgDefault = 'img/default.png'
+      const target = document.getElementsByClassName(lazyLoadImg.config.class_target)
+      const imgDefault = lazyLoadImg.config.image_default
       for (const item of target) {
         const imgOrigin = item.getAttribute('src')
         item.setAttribute('src', imgDefault)
         item.setAttribute('data-src', imgOrigin)
       }
     },
+    // scrollに伴うlazy load 制御処理
+    main: function() {
+      // 処理
+      const target = document.getElementsByClassName(lazyLoadImg.config.class_target)
+      const winHeight = window.parent.screen.height
+      const offsetHeight = lazyLoadImg.config.offset_y_loaded
+      for (const item of target) {
+        const isLoaded = item.classList.contains(lazyLoadImg.config.class_loaded)
+        // lazy load 済みの画像はスキップする
+        if (isLoaded) {
+          continue
+        }
+        // スクロール位置が近づくまでスキップする
+        const objectY = item.getBoundingClientRect().top
+        if (objectY > winHeight + offsetHeight) {
+          return
+        }
+        // lazy load処理へ
+        lazyLoadImg.loadLazy(item)
+      }
+
+      // すべてload済みになったら、removeEventListenerによりイベントをunbind
+      lazyLoadImg.eventUnBind()
+    },
     // lazy load処理
     loadLazy: function(elm) {
       const imgCurrent = elm.getAttribute('data-src')
       elm.setAttribute('src', imgCurrent)
-      elm.classList.add('js-loaded')
-    },
-    // scrollに伴うlazy load処理
-    main: function() {
-      const that = this
-
-      // 処理
-      const target = document.getElementsByClassName(lazyLoadImg.config.target_class_name)
-      for (const item of target) {
-        const isLoaded = item.hasAttribute('data-loaded')
-        if (isLoaded) {
-          return
-        }
-        that.loadLazy(item)
-      }
-
-      // removeEventListener
-      window.removeEventListener('scroll', lazyLoadImg.main)
+      elm.classList.add(lazyLoadImg.config.class_loaded)
     },
   }
 
